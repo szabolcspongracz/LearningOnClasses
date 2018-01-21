0bc3011c5ba0dfc569034367accae36c72cdda5b
pongi <vegyeszpongi@gmail.com>
2018-01-21 14:44:46 +0100

    costructors added

--- src/project1/Cat.java
+++ src/project1/Cat.java
@@ -3,6 +3,19 @@ package project1;
 
 public class Cat extends Animal implements Pet{
     
+    public Cat(){
+        super();
+    }
+    
+    public Cat(String name){
+        this.setName(name);
+    }
+     
+    public Cat(String name, int weight){
+        this.setName(name);
+        this.setWeight(weight);
+    }
+    
     @Override
     public void makeSound(){
     System.out.println("MEOW");
