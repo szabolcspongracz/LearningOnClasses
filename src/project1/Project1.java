710c475e3705a6d213cc61058e57247c8eee766e
pongi <vegyeszpongi@gmail.com>
2018-01-21 11:54:26 +0100

    öröklődés bevezetése

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -5,14 +5,7 @@ public class Project1 {
 
   
     public static void main(String[] args) {
-        Human first = new Human();
-        String thing = "Alma";
-        
-//        System.out.println( first.getName() == null ? "Üres" : "Nem üres, az értéke " + first.getName());
-//       if (first.getName() == null){
-//           System.out.println("Nincs név!");
-//       }else{
-//           System.out.println("Van név: " + first.getName());
-//       }
+        Cat macska = new Cat();
+        macska.makeSound();
     }
 }
