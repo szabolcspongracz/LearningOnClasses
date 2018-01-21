d44161462b07215f04cf13adb81227f997effe2d
pongi <vegyeszpongi@gmail.com>
2018-01-21 11:54:41 +0100

    öröklődés bevezetése

---
+++ src/project1/Animal.java
@@ -0,0 +1,29 @@
+
+package project1;
+
+
+public class Animal {
+    private String name;
+    private int weight;
+    
+    public void makeSound(){
+        System.out.println("AAAAAAAAA");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    
+}
---
+++ src/project1/Cat.java
@@ -0,0 +1,10 @@
+
+package project1;
+
+public class Cat extends Animal{
+    
+    @Override
+    public void makeSound(){
+    System.out.println("MEOW");
+    }
+}
